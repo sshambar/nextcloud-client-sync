6d192fe6b1a8040653c1c2eebfb89c86aabb8e90
Scott Shambarger <devel@shambarger.net>
2024-07-09 12:42:48 -0700

    Added support for email on error and logging to local file w/ rotation

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # use base image from Docker Hub and upgrade existing packages
-FROM nextcloud-client-base:latest AS base
+FROM nextcloud-client-base:latest
 
 # Set container label
 LABEL org.opencontainers.image.title="Nextcloud Client Sync Image" \
@@ -12,11 +12,20 @@ ENV NC_URL="" \
     NC_NETRC="/netrc" \
     NC_EXCLUDE="/sync-exclude.lst" \
     NC_DATADIR="/data" \
+    NC_LOG_FILE="" \
+    NC_LOG_MAX_BYTES=1048576 \
+    NC_LOG_ARCHIVES=4 \
     NC_OPTIONS="" \
     NC_VERBOSE="" \
-    NC_RESYNC_SECS="" \
-    NC_FAIL_RETRY_SECS=300
+    NC_RESTART_SECS="" \
+    NC_FAIL_RETRY_SECS=300 \
+    NC_ERROR_TO="" \
+    NC_SENDMAIL_OPTIONS="" \
+    NC_ERROR_FROM="" \
+    NC_ERROR_SUBJECT="" \
+    NC_ERROR_COUNT=""
 
 COPY entrypoint.sh /entrypoint.sh
+RUN chmod a+rx /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,51 @@
 #!/bin/bash
+#
+# nextcloud-client-sync container entrypoint.sh v1.0
+# Author: Scott Shambarger <devel@shambarger.net>
+#
+# Copyright (C) Scott Shambarger. All rights reserved.
+# SPDX-License-Identifier: GPL-2.0-or-later
 
-log() {
-  echo "$(date -D "%m-%d %H:%M:%S"): $*"
+log() { echo "$(date -D "%m-%d %H:%M:%S"): $*"; }
+err() { log >&2 "$*"; }
+fatal() { err "$*"; exit 1; }
+
+to_int() { # <var>
+  printf 2>/dev/null -v "$1" "%d" "${!1}" || printf -v "$1" 0
 }
 
-fatal() {
-  log "$*"
-  exit 1
+# sanity check
+to_int NC_LOG_MAX_BYTES
+(( NC_LOG_MAX_BYTES < 20000 )) && NC_LOG_MAX_BYTES=20000
+to_int NC_LOG_ARCHIVES
+(( NC_LOG_ARCHIVES < 1 )) && NC_LOG_ARCHIVES=1
+
+log_rotate() {
+  local i d=$NC_LOG_FILE
+  [[ $d ]] || return
+  if [[ $(find . -maxdepth 1 -name "$d" -size "+${NC_LOG_MAX_BYTES}c") ]]; then
+    for ((i==NC_LOG_ARCHIVES-1; i>0; i--)); do
+      [[ -f "$d.$i" ]] && mv "$d."{$i,$((i+1))}
+    done
+    mv "$d" "$d.1"
+    exec > "$d"
+  fi
 }
+
+if [[ $NC_LOG_FILE ]] && touch "$NC_LOG_FILE" && [[ -w $NC_LOG_FILE ]]; then
+  exec >> "$NC_LOG_FILE"
+  exec 2> >(while read -r
+            do echo >&2 "$REPLY"
+               echo "$REPLY" >> "$NC_LOG_FILE"
+            done)
+else
+  log "Unable to access log file '$NC_LOG_FILE'"
+  NC_LOG_FILE=
+fi
+
+error_log=/tmp/cmd.log
+NC_ERROR_FROM=${NC_ERROR_FROM:-nextcloud-client-sync}
+NC_ERROR_SUBJECT=${NC_ERROR_SUBJECT:-nextcloud-client-sync failed}
 
 log "Starting up..."
 
@@ -35,27 +73,60 @@ if [[ -f $NC_EXCLUDE ]]; then
   NC_ARGS+=(--exclude "$NC_EXCLUDE")
 fi
 
-[[ $NC_VERBOSE ]] || NC_ARGS+=(-s)
+[[ -z $NC_VERBOSE && -z $NC_ERROR_TO ]] && NC_ARGS+=(-s)
 [[ $NC_PATH ]] && NC_ARGS+=(--path "$NC_PATH")
+
+fail_count=0
 
 while :; do
 
+  log_rotate
   log "Running nextcloudcmd"
-  nextcloudcmd ${NC_OPTIONS} "${NC_ARGS[@]}" "$NC_DATADIR" "$NC_URL"
+  if [[ $NC_ERROR_TO ]]; then
+    nextcloudcmd &>"$error_log" ${NC_OPTIONS} "${NC_ARGS[@]}" "$NC_DATADIR" "$NC_URL"
+  else
+    nextcloudcmd ${NC_OPTIONS} "${NC_ARGS[@]}" "$NC_DATADIR" "$NC_URL"
+  fi
   rc=$?
 
-  delay=$NC_RESYNC_SECS
+  delay=$NC_RESTART_SECS
+
   if [[ $rc -ne 0 ]]; then
-    log nextcloudcmd ${NC_OPTIONS} "${NC_ARGS[@]}" "$NC_DATADIR" "$NC_URL"
-    log " command failed ($rc)"
+    if (( fail_count++ == 0 )); then
+      # log details
+      err nextcloudcmd ${NC_OPTIONS} "${NC_ARGS[@]}" "$NC_DATADIR" "$NC_URL"
+      err " failed: err $rc"
+      err "$(<"$error_log")"
+    else
+      err "nextcloudcmd failure $fail_count: err $rc"
+    fi
     [[ $delay && $NC_FAIL_RETRY_SECS ]] && delay=$NC_FAIL_RETRY_SECS
+
+    if [[ $NC_ERROR_TO && $fail_count == "${NC_ERROR_COUNT:-1}" ]]; then
+      err "Sending error email to $NC_ERROR_TO"
+      sendmail $NC_SENDMAIL_OPTIONS -f "$NC_ERROR_FROM" $NC_ERROR_TO <<EOF
+From: $NC_ERROR_FROM
+To: $NC_ERROR_TO
+Subject: $NC_ERROR_SUBJECT
+Date: $(date -R)
+
+Command failed $fail_count times (error $rc):
+nextcloudcmd ${NC_OPTIONS} "${NC_ARGS[@]}" "$NC_DATADIR" "$NC_URL"
+
+Error log:
+$(<"$error_log")
+
+EOF
+    fi
+  else
+    fail_count=0
   fi
   # if not looping, exit
   [[ $delay && $delay -gt 0 ]] || exit $rc
 
   # sleep before resync
   log "Sleeping $delay secs"
-  sleep $delay
+  sleep "$delay"
 done
 
 exit 0
